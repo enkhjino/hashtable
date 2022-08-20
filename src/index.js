502942f751846640c25fcf76932adf29ee2f4a93
enkhjino <96076106+enkhjino@users.noreply.github.com>
2022-08-20 00:00:53 -0700

    _hash function

--- src/index.js
+++ src/index.js
@@ -14,6 +14,17 @@ console.log(hash("pink", 10));
 // console.log(hash("yellow", 10))
 
 class HashTable {
-  constructor(size=53){
+  constructor(size = 53) {
     this.keyMap = new Array(size);
   }
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+}
